457a8f71786bb7dbb993e324bbf1c4797db4e615
Jakub Kalinowski <jakubjankalinowski@gmail.com>
2022-07-17 18:06:06 +0200

    added show page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Show from './pages/Show';
 import Starred from './pages/Starred';
 
 function App() {
@@ -9,6 +10,10 @@ function App() {
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+
+      <Route exact path="/show/:id">
+        <Show />
       </Route>
 
       <Route exact path="/starred">
